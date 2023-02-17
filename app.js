3228111ce2a1c29ddc9fd82d90981a27ece09bf5
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2023-02-17 03:14:44 +0300

    fixed mongoose

--- app.js
+++ app.js
@@ -32,6 +32,22 @@ const corsOptions = {
   credentials: true,
 };
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(NODE_ENV === 'production' ? process.env.DB_CONNECTION_STRING : 'mongodb://127.0.0.1/bitfilmsdb', {
+      useNewUrlParser: true,
+    }, () => {
+      app.listen(PORT, () => {
+        console.log(`App works, port ${PORT}`);
+      });
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 app.use(requestLogger);
 app.use(limiter);
 app.use(cors(corsOptions));
@@ -51,10 +67,9 @@ app.use(errors());
 app.use(errorsHandler);
 
 mongoose.set('strictQuery', false);
-mongoose.connect(NODE_ENV === 'production' ? process.env.DB_CONNECTION_STRING : 'mongodb://127.0.0.1/bitfilmsdb', {
-  useNewUrlParser: true,
-}, () => {
+
+connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`App works, port ${PORT}`);
+    console.log('listening for requests');
   });
 });
