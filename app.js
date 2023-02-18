c7ef8abd9289a13abb5d2d8b2a542ba178aaba21
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2023-02-19 01:40:19 +0300

    fixed mongoose

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const connectDB = async () => {
   try {
     // const conn = await mongoose.connect('mongodb://127.0.0.1/bitfilmsdb');
     const conn = await mongoose.connect(
-      'mongodb+srv://kharkovchenko:Mongobd123@cluster0.l1pn8hw.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://kharkovchenko:Mongodb123@cluster0.l1pn8hw.mongodb.net/?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
